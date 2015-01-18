77d39ca54cd374d0054ce40220ec64f185d53bbe
Edoardo Scibona <scibona.edoardo@gmail.com>
2015-01-18 17:49:55 +0100

    Added register method.

--- meteocal-se2/src/main/java/it/polimi/se2/meteocal/gui/access/RegistrationBean.java
+++ meteocal-se2/src/main/java/it/polimi/se2/meteocal/gui/access/RegistrationBean.java
@@ -6,32 +6,42 @@
 package it.polimi.se2.meteocal.gui.access;
 
 import it.polimi.se2.meteocal.entity.User;
+import it.polimi.se2.meteocal.manager.UserManager;
+import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
 
 /**
- *JSF managed bean for the registration page.
+ * JSF managed bean for the registration page.
+ *
  * @author edo
  */
 @Named(value = "registrationBean")
 @RequestScoped
 public class RegistrationBean {
 
-    private User user;
+    @EJB
+    private UserManager um;
     
+    private User user;
+
     public RegistrationBean() {
     }
     
+    public String register() {
+        um.saveUser(user);
+        return "/personalPages/personalPage";
+    }
+
     public User getUser() {
         if (user == null) {
             user = new User();
         }
         return user;
     }
-    
+
     public void setUser(User user) {
         this.user = user;
     }
-    
-    
+
 }
