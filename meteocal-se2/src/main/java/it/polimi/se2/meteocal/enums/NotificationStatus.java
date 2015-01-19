17b6aef125e7ab18baf7420ac9c8bcb3e690c109
Edoardo Scibona <scibona.edoardo@gmail.com>
2015-01-19 19:50:19 +0100

    Added enums for the notification's type and status and for the weather condition.

---
+++ meteocal-se2/src/main/java/it/polimi/se2/meteocal/enums/NotificationStatus.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.polimi.se2.meteocal.enums;
+
+/**
+ *Enum with all the possible states for a Notification
+ * @author edo
+ */
+public enum NotificationStatus {
+    PENDING,
+    READ,
+    ACCEPTED,
+    DECLINED
+}
---
+++ meteocal-se2/src/main/java/it/polimi/se2/meteocal/enums/NotificationType.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.polimi.se2.meteocal.enums;
+
+/**
+ *Enum for the possible types of a Notification
+ * @author edo
+ */
+public enum NotificationType {
+    SIMPLE,
+    INVITE,
+    UPDATE,
+    DELETE,
+    WEATHER
+}
