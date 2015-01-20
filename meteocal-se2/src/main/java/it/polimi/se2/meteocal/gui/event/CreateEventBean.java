f45146f8db31e5684be91f0ab9d7b0f8a0e699c6
Edoardo Scibona <scibona.edoardo@gmail.com>
2015-01-20 16:51:02 +0100

    Added createEventBean.

---
+++ meteocal-se2/src/main/java/it/polimi/se2/meteocal/gui/event/CreateEventBean.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.polimi.se2.meteocal.gui.event;
+
+import it.polimi.se2.meteocal.entity.Event;
+import java.util.logging.Logger;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+/**
+ * JSF managed bean for the event creation
+ *
+ * @author edo
+ */
+@Named(value = "createEventBean")
+@RequestScoped
+public class CreateEventBean {
+
+    private static final Logger logger = Logger.getLogger("createEventBean");
+
+    private Event event;
+
+    public CreateEventBean() {
+    }
+
+    public Event getEvent() {
+        if (event == null) {
+            event = new Event();
+        }
+        return event;
+    }
+
+    public void setEvent(Event event) {
+        this.event = event;
+    }
+
+}
