223ad1c39464c7bc97e75cc06b1e50ca630c943f
Edoardo Scibona <scibona.edoardo@gmail.com>
2015-01-21 22:19:32 +0100

    Added getScheduledEvents method. Added documentation.

--- meteocal-se2/src/main/java/it/polimi/se2/meteocal/manager/UserManager.java
+++ meteocal-se2/src/main/java/it/polimi/se2/meteocal/manager/UserManager.java
@@ -5,13 +5,19 @@
  */
 package it.polimi.se2.meteocal.manager;
 
+import it.polimi.se2.meteocal.entity.Event;
 import it.polimi.se2.meteocal.entity.Group;
 import it.polimi.se2.meteocal.entity.User;
+import it.polimi.se2.meteocal.utilities.DateHelper;
+import java.util.ArrayList;
+import java.util.List;
 import javax.annotation.Resource;
 import javax.ejb.SessionContext;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import org.primefaces.model.DefaultScheduleEvent;
+import org.primefaces.model.ScheduleEvent;
 
 /**
  * EJB managing the users.
@@ -27,11 +33,19 @@ public class UserManager {
     @Resource
     private SessionContext sctx;
 
+    /**
+     * Save a user in the DB
+     * @param user the new user to add to the DB
+     */
     public void saveUser(User user) {
         user.setGroupName(Group.USERS);
         em.persist(user);
     }
 
+    /**
+     *
+     * @return the logged User object
+     */
     public User getLoggedUser() {
         User u = em.createNamedQuery(User.findByUsername, User.class)
                 .setParameter("username", getLoggedUserName())
@@ -39,8 +53,30 @@ public class UserManager {
         return u;
     }
 
+    /**
+     *
+     * @return the logged user's username
+     */
     public String getLoggedUserName() {
         return sctx.getCallerPrincipal().getName();
+    }
+
+    /**
+     *
+     * @return the scheduled events that the user will be attending ready to be used by primefaces schedule
+     */
+    public List<ScheduleEvent> getScheduledEvents() {
+        List<ScheduleEvent> eventList = new ArrayList<>();
+        User u = getLoggedUser();
+        ScheduleEvent scheduleEvent;
+        //For each event that the user will be attending we build the corrisponding schedule event for display
+        for (Event e : u.getAttendingEvents()) {
+            scheduleEvent = new DefaultScheduleEvent(e.getName(),
+                    DateHelper.buildDate(e.getEventDate(), e.getStartingTime()),
+                    DateHelper.buildDate(e.getEventDate(), e.getEndingTime()));
+            eventList.add(scheduleEvent);
+        }
+        return eventList;
     }
 
 }
