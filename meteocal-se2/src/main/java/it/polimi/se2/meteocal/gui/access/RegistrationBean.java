8e1437c048040989e2e4b79d6d10313d64b32f80
Edoardo Scibona <scibona.edoardo@gmail.com>
2015-01-21 18:34:46 +0100

    Changed register method to permit automatic login after a successful registration.

--- meteocal-se2/src/main/java/it/polimi/se2/meteocal/gui/access/RegistrationBean.java
+++ meteocal-se2/src/main/java/it/polimi/se2/meteocal/gui/access/RegistrationBean.java
@@ -8,8 +8,9 @@ package it.polimi.se2.meteocal.gui.access;
 import it.polimi.se2.meteocal.entity.User;
 import it.polimi.se2.meteocal.manager.UserManager;
 import javax.ejb.EJB;
-import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
 
 /**
  * JSF managed bean for the registration page.
@@ -23,14 +24,28 @@ public class RegistrationBean {
     @EJB
     private UserManager um;
     
+    @Inject
+    private LoginBean lb;
+    
+    //Needed for the automatic login after registration
+    private String username;
+    private String password;
+    
     private User user;
 
     public RegistrationBean() {
     }
     
+    /**
+     * Registers the new user and logs it in
+     * @return the user's personal page if the registration is successful
+     */
     public String register() {
+        getUser();
+        user.setUsername(username);
+        user.setPassword(password);
         um.saveUser(user);
-        return "/personalPages/personalPage";
+        return lb.login(this.username, this.password);
     }
 
     public User getUser() {
@@ -44,4 +59,20 @@ public class RegistrationBean {
         this.user = user;
     }
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
 }
