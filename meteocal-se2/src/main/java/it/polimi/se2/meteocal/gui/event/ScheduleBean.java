9df29e5283a6e01816adc79e8ee12a123e959c7e
Edoardo Scibona <scibona.edoardo@gmail.com>
2015-01-23 21:31:20 +0100

    Modified init added onEventSelectListener.

--- meteocal-se2/src/main/java/it/polimi/se2/meteocal/gui/event/ScheduleBean.java
+++ meteocal-se2/src/main/java/it/polimi/se2/meteocal/gui/event/ScheduleBean.java
@@ -5,43 +5,60 @@
  */
 package it.polimi.se2.meteocal.gui.event;
 
+import it.polimi.se2.meteocal.entity.Event;
 import it.polimi.se2.meteocal.manager.UserManager;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
-import javax.enterprise.context.RequestScoped;
-import javax.inject.Named;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import org.primefaces.event.SelectEvent;
 import org.primefaces.model.DefaultScheduleModel;
+import org.primefaces.model.ScheduleEvent;
 import org.primefaces.model.ScheduleModel;
 
 /**
- *Backing bean for the primefaces schedule component
- * used in the user personal page
+ * Backing bean for the primefaces schedule component used in the user personal
+ * page
+ *
  * @author edo
  */
-@Named(value = "scheduleBean")
-@RequestScoped
-public class ScheduleBean {
+@ManagedBean(name = "scheduleBean")
+@ViewScoped
+public class ScheduleBean implements Serializable {
 
     private ScheduleModel eventModel;
-    
+    private ScheduleEvent scheduleEvent;
+
+    private Map<ScheduleEvent, Event> eventsMap;
+    private List<ScheduleEvent> scheduleEventsList;
+
+    private Event event;
+
     @EJB
     private UserManager um;
-    
+
     @PostConstruct
-    public void populateSchedule() {
-        eventModel = new DefaultScheduleModel(um.getScheduledEvents());
+    public void init() {
+        eventsMap = um.getEventsMap();
+        scheduleEventsList = new ArrayList<>(eventsMap.keySet());
+        eventModel = new DefaultScheduleModel(scheduleEventsList);
+    }
+
+    public void onEventSelect(SelectEvent selectedEvent) {
+        scheduleEvent = (ScheduleEvent) selectedEvent.getObject();
+        event = eventsMap.get(scheduleEvent);
     }
 
     public ScheduleModel getEventModel() {
         return eventModel;
     }
 
-    public void setEventModel(ScheduleModel eventModel) {
-        this.eventModel = eventModel;
+    public Event getEvent() {
+        return event;
     }
-            
-            
-    public ScheduleBean() {
-    }
-    
+
 }
