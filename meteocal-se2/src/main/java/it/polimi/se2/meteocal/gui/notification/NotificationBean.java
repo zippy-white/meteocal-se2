56c06a6da1d44ae94d114da5f59ead512bbb40c9
Edoardo Scibona <scibona.edoardo@gmail.com>
2015-01-25 16:44:54 +0100

    Added various methods and listeners.

--- meteocal-se2/src/main/java/it/polimi/se2/meteocal/gui/notification/NotificationBean.java
+++ meteocal-se2/src/main/java/it/polimi/se2/meteocal/gui/notification/NotificationBean.java
@@ -5,13 +5,21 @@
  */
 package it.polimi.se2.meteocal.gui.notification;
 
+import it.polimi.se2.meteocal.entity.Notification;
+import it.polimi.se2.meteocal.enums.NotificationStatus;
 import it.polimi.se2.meteocal.manager.UserManager;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
 /**
+ * The JSF managed bean that operates the notification panel
  *
  * @author edo
  */
@@ -22,7 +30,51 @@ public class NotificationBean implements Serializable {
     @EJB
     private UserManager um;
 
-    public NotificationBean() {
+    private List<Notification> userNotifications;
+    private List<NotificationView> pendingNotifications;
+    private Map<NotificationView, Notification> notificationsMap;
+    private NotificationView selectedNotification;
+
+    @PostConstruct
+    public void init() {
+        userNotifications = new ArrayList<>(um.getLoggedUser().getNotifications());
+        buildViewNotifications();
+        pendingNotifications = new ArrayList<>(notificationsMap.keySet());
+
+    }
+
+    public void acceptInvite() {
+
+    }
+
+    public void declineInvite() {
+
+    }
+
+    public void markAsRead() {
+
+    }
+
+    public List<NotificationView> getPendingNotifications() {
+        return pendingNotifications;
+    }
+
+    private void buildViewNotifications() {
+        notificationsMap = new HashMap<>();
+        for (Notification n : userNotifications) {
+            if (n.getStatus() == NotificationStatus.PENDING) {
+                NotificationView nv = new NotificationView(n.getId().toString(), n.getType(), n.getGeneratingEventName(), n.getGeneratingEventDetails());
+                notificationsMap.put(nv, n);
+            }
+        }
+    }
+
+    public NotificationView getSelectedNotification() {
+        return selectedNotification;
+    }
+
+    public void setSelectedNotification(NotificationView selectedNotification) {
+        this.selectedNotification = selectedNotification;
     }
 
 }
