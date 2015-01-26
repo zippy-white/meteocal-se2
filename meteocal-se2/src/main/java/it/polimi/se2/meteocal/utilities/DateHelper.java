6a2605f78297ceec66eb99b743debe0510e2ceac
Edoardo Scibona <scibona.edoardo@gmail.com>
2015-01-26 17:30:39 +0100

    Added getTodaysDate(), improved documentation.

--- meteocal-se2/src/main/java/it/polimi/se2/meteocal/utilities/DateHelper.java
+++ meteocal-se2/src/main/java/it/polimi/se2/meteocal/utilities/DateHelper.java
@@ -29,22 +29,42 @@ public class DateHelper {
         t.setTime(rawDate);
         return t.getTime();
     }
-    
+
     /**
      * Check if to events, represented by time windows overlap
+     *
      * @param one the first time window
      * @param two the second time window
      * @return true if the time windows overlap false otherwise
      */
     public static Boolean overlaps(TimeWindow one, TimeWindow two) {
-        return !((one.getStart().before(two.getStart()) &&
-                one.getEnd().before(two.getStart())) ||
-                (one.getStart().after(two.getEnd()) &&
-                one.getEnd().after(two.getEnd())));
+        return !((one.getStart().before(two.getStart())
+                && one.getEnd().before(two.getStart()))
+                || (one.getStart().after(two.getEnd())
+                && one.getEnd().after(two.getEnd())));
     }
-    
+
+    /**
+     * 
+     * @param day the day we want to check
+     * @param hour the hour of the day
+     * @return true if the day plus hour date is before now
+     */
     public static Boolean scheduledInThePast(Date day, Date hour) {
         Date date = buildDate(day, hour);
         return date.before(Calendar.getInstance().getTime());
+    }
+
+    /**
+     * 
+     * @return only today's date without hours, minutes, seconds and milliseconds.
+     */
+    public static Date getTodaysDate() {
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.HOUR_OF_DAY, 0);
+        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.SECOND, 0);
+        cal.set(Calendar.MILLISECOND, 0);
+        return cal.getTime();
     }
 }
